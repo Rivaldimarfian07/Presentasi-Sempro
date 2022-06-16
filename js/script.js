b723e4b056c986db6c2e8daca9303190eaea2ed3
oxlord <rivaldimarfian07@gmail.com>
2022-06-16 22:29:44 +0700

    update (commit "on main")

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // typing animasi
 var typed = new Typed(".typing",{
-  strings:["Mahasiswa universitas jabal ghafur","fakultas teknik informatika","Become one with my code"],
+  strings:["Mahasiswa universitas jabal ghafur","Fakultas Teknik Informatika","Become One With My Code"],
   typedSpeed:70,
   backSpeed:60,
   loop:true
