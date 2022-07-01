253849fede85214c3d15f23cb92ab962f673bdde
Rivaldimarfian <rivaldimarfian07@gmail.com>
2022-07-01 11:52:52 +0700

    final update

---
+++ js/style-switch.js
@@ -0,0 +1,30 @@
+// =============== style toggler
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click",() =>{
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
+
+// hide style - switcher on scroll
+
+window.addEventListener("scroll", () => {
+  if(document.querySelector(".style-switcher").classList.contains("open"));
+  {
+    document.querySelector(".style-switcher").classList.remove("open");
+  }
+})
+
+// =============== theme colors
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color)
+{
+    alternateStyles.forEach((style) => {
+      if(color === style.getAttribute("title"))
+      {
+        style.removeAttribute("disabled");
+      }
+      else
+      {
+        style.setAttribute("disabled","true");
+      }
+    })
+}
